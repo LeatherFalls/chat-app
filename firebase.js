16457af9471e42feb5aa6cda74596f355fac3d81
Vinicius Gomes <viniplay0599@gmail.com>
2023-06-01 19:35:10 -0300

    config: implements firebase config file

---
+++ firebase.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+import 'firebase/compat/firestore';
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+
+const auth = app.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
